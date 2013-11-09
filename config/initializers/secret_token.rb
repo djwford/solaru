112a69623edbdc9fe4dd339682ba3c59cec4e30c
Derek <derekweatherford@gmail.com>
2013-11-09 10:57:45 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Solaru::Application.config.secret_token = '94a3d78866e474335aacf6a2e8b0347bf3f4421bcdd40d2b2a16376f3fac70008cd658a484875a80accec816ab9cc645ca288802c0c3b65fefb95791b25f8ce7'
